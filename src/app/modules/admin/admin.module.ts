73e5d0b149de16255e4e3047d3f86bac074bdda9
di3gokanon89 <diego.paniagua.kanon89@gmail.com>
2022-06-23 13:41:01 -0500

    Setup Angular Material in LOTR project

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -2,13 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { adminRouter } from './admin-routing.module';
 import { CharactersComponent } from './components/characters/characters.component';
+import { MatSliderModule } from '@angular/material/slider';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     CharactersComponent
   ],
   imports: [
-    adminRouter
+    adminRouter,
+    MatSliderModule,
   ],
   providers: [],
   bootstrap: [CharactersComponent]
